aca3e3e24e881a44fb569666e57b46e2b313d507
ZARIFMAC <uzbzarif@gmail.com>
2023-01-17 10:00:30 +0500

    add databse

---
+++ db.py
@@ -0,0 +1,30 @@
+import json
+
+class DB:
+    def __init__(self,path):
+        self.path = path
+        # Read the data from the file
+        self.data = {}
+        with open(self.path,'r') as f:
+            self.data = json.load(f)
+
+
+    def getPhone(self,brand):
+        """
+        Return phone data by brand
+        args:
+            brand: str
+        return:
+            dict
+        """
+        data = {
+            'model':'',
+            'color':'',
+            'ram':'',
+            'price':'',
+            'memory':'',
+            'image':'',
+        }
+
+        
+  
