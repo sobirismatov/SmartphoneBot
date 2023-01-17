1edbc1cff1325c16bf862d09e1bcb348be81e018
ZARIFMAC <uzbzarif@gmail.com>
2023-01-17 09:55:49 +0500

    send iphone

--- main.py
+++ main.py
@@ -85,14 +85,58 @@ def photo(update: Update, context: CallbackContext):
     bot = context.bot
     bot.sendMessage(chat_id=chat_id,text='Photo qabul qilindi')
 
+def shop(update: Update, context: CallbackContext):
+    chat_id = update.message.chat.id
+
+    iphone = InlineKeyboardButton(text='Iphone',callback_data='phone iphone')
+    samsung = InlineKeyboardButton(text='Samsung',callback_data='phone samsung')
+    huawei = InlineKeyboardButton(text='Huawei',callback_data='phone huawei')
+    xiaomi = InlineKeyboardButton(text='Xiaomi',callback_data='phone xiaomi')
+    vivo = InlineKeyboardButton(text='Vivo',callback_data='phone vivo')
+    oppo = InlineKeyboardButton(text='Oppo',callback_data='phone oppo')
+
+    # Define keyboard
+    keyboar = InlineKeyboardMarkup([
+        [iphone,samsung,huawei],
+        [xiaomi,vivo,oppo]
+    ])
+
+    bot = context.bot
+    bot.sendMessage(chat_id=chat_id,text='Choose a phone',reply_markup=keyboar)
+
+def phone(update: Update, context: CallbackContext):
+    query = update.callback_query
+    chat_id = query.message.chat_id
+    data,brand = query.data.split()
+
+    bot = context.bot
+    if brand=='iphone':
+        model= 'iPhone XR'
+        color = 'Black'
+        ram= '4GB'
+        memory= '64GB'
+        price = '400$'
+        img_url ='https://images-na.ssl-images-amazon.com/images/I/51qBzX0pGYL._SL1000_.jpg'
+        text = f'Phone model: {model}\nColor: {color}\nRAM: {ram}\nMemory: {memory}\nPrice: {price}'
+        bot.sendPhoto(chat_id=chat_id,caption=text,photo=img_url)
+        
+
+        
+
+
+    query.answer('Phone')
+    print(brand)
+
 updater = Updater(token=TOKEN)
 
 updater.dispatcher.add_handler(CommandHandler('start',start))
 # Add handler for photo message
 updater.dispatcher.add_handler(MessageHandler(Filters.photo,photo))
+updater.dispatcher.add_handler(MessageHandler(Filters.text('🛍 Shop'),shop))
 updater.dispatcher.add_handler(MessageHandler(Filters.text('📝 About'),about))
 updater.dispatcher.add_handler(MessageHandler(Filters.text('📞 Contact'),contact))
 updater.dispatcher.add_handler(MessageHandler(Filters.text('Main menu'),start))
+updater.dispatcher.add_handler(CallbackQueryHandler(phone,pattern='phone'))
 updater.dispatcher.add_handler(CallbackQueryHandler(query))
 
 updater.start_polling()
